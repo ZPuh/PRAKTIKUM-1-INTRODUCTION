e67db74e07c840387ce64b51804d22cc3f266516
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-03-04 15:22:28 +0800

    (6/16)

---
+++ No06B.java
@@ -0,0 +1,22 @@
+package Praktikum01;
+
+import java.util.Scanner;
+
+public class No06B {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("m : ");
+        int m = input.nextInt();
+        System.out.print("n : ");
+        int n = input.nextInt();
+        
+        int max = Math.max(m, n);
+        int min = Math.min(m, n);
+        
+        System.out.println("Min : "+min);
+        System.out.println("MAx : "+max);
+        
+    }
+
+}
