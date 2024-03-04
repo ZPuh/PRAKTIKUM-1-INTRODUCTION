defc3a29dc2be3f720e42a8b38dd1530f1a63132
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-03-04 19:15:07 +0800

    object 6

---
+++ no01/laptop.java
@@ -0,0 +1,14 @@
+
+public class laptop {
+
+    String merek;
+    String spek;
+
+    void hidup() {
+        System.out.println("MAri coding");
+    }
+
+    void lobet() {
+        System.out.println("Cass!!!!");
+    }
+}
