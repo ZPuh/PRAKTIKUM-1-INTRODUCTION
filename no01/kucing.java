bc146136eda4477392ed037133380ae8b3fd0af1
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-03-04 19:13:30 +0800

    object 3

---
+++ no01/kucing.java
@@ -0,0 +1,14 @@
+
+public class kucing {
+
+    String warna;
+    String jenis;
+
+    void memakan() {
+        System.out.println("Ikan");
+    }
+
+    void meminum() {
+        System.out.println("Susu");
+    }
+}
