5bc787d0db74d5104b85802beaf2449387078cba
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-03-04 19:18:23 +0800

    object 7

---
+++ no01/mobil.java
@@ -0,0 +1,14 @@
+
+public class mobil {
+
+    String merek;
+    String warna;
+
+    void mengerem() {
+        System.out.println("Berhenti");
+    }
+
+    void mengegas() {
+        System.out.println("Maju");
+    }
+}
