b2d89cb622453cc639612bae63be66567fab15ce
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-03-04 19:14:38 +0800

    object 5

---
+++ no01/lampu.java
@@ -0,0 +1,14 @@
+
+public class lampu {
+
+    String merek;
+    String warna;
+
+    void menyala() {
+        System.out.println("Terang");
+    }
+
+    void redup() {
+        System.out.println("Gelap");
+    }
+}
