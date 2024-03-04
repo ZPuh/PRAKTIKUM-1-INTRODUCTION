e4f0f399d7216975ef4fc007d820c57243aac5cc
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-03-04 19:20:16 +0800

    object 10

---
+++ no01/ular.java
@@ -0,0 +1,13 @@
+public class ular {
+    String warna;
+    int panjang;
+    String jenis;
+    
+    void bergerak (){
+        System.out.println("Kedepan");
+    }
+    void megigit(){
+        System.out.println("MAnusia");
+    }
+    
+}
