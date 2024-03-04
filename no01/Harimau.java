5fd0c48102a346d95db51e14c634787c248bc6a0
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-03-04 19:12:59 +0800

    object 2

---
+++ no01/Harimau.java
@@ -0,0 +1,13 @@
+
+public class Harimau {
+
+    String warna;
+    int usia;
+
+    void bergerak(){
+        System.out.println("Kedepan");
+}
+    void memakan(){
+        System.out.println("Rusa");
+    }
+}
