6bcca3b315c5a26d28e9413af2a98f39560ff175
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-03-04 18:42:33 +0800

    (9/16)

---
+++ no09B.java
@@ -0,0 +1,419 @@
+package Praktikum01;
+
+public class no09B {
+
+    // instan variabel
+    String Varinst1;
+    int Varinst2;
+    byte Varinst3;
+    short Varinst4;
+    double Varinst5;
+    Integer Varinst6;
+    Double Varinst7;
+    Byte Varinst8;
+    Short Varinst9;
+    String Varinst10;
+    int Varinst11;
+    byte Varinst12;
+    short Varinst13;
+    double Varinst14;
+    Integer Varinst15;
+    Double Varinst16;
+    Byte Varinst17;
+    Short Varinst18;
+    String Varinst19;
+    int Varinst20;
+    byte Varinst21;
+    short Varinst22;
+    double Varinst23;
+    Integer Varinst24;
+    Double Varinst25;
+    Byte Varinst26;
+    Short Varinst27;
+    String Varinst28;
+    int Varinst29;
+    byte Varinst30;
+    short Varinst31;
+    double Varinst32;
+    Integer Varinst33;
+    Double Varinst34;
+    Byte Varinst35;
+    Short Varinst36;
+    String Varinst37;
+    int Varinst38;
+    byte Varinst39;
+    short Varinst40;
+    double Varinst41;
+    Integer Varinst42;
+    Double Varinst43;
+    Byte Varinst44;
+    Short Varinst45;
+    String Varinst46;
+    int Varinst47;
+    byte Varinst48;
+    short Varinst49;
+    double Varinst50;
+    Integer Varinst51;
+    Double Varinst52;
+    Byte Varinst53;
+    Short Varinst54;
+    String Varinst55;
+    int Varinst56;
+    byte Varinst57;
+    short Varinst58;
+    double Varinst59;
+    Integer Varinst60;
+    Double Varinst61;
+    Byte Varinst62;
+    Short Varinst63;
+    String Varinst64;
+    int Varinst65;
+    byte Varinst66;
+    short Varinst67;
+    double Varinst68;
+    Integer Varinst69;
+    Double Varinst70;
+    Byte Varinst71;
+    Short Varinst72;
+    String Varinst73;
+    int Varinst74;
+    byte Varinst75;
+    short Varinst76;
+    double Varinst77;
+    Integer Varinst78;
+    Double Varinst79;
+    Byte Varinst80;
+    Short Varinst81;
+    String Varinst82;
+    int Varinst83;
+    byte Varinst84;
+    short Varinst85;
+    double Varinst86;
+    Integer Varinst87;
+    Double Varinst88;
+    Byte Varinst89;
+    Short Varinst90;
+    String Varinst91;
+    int Varinst92;
+    byte Varinst93;
+    short Varinst94;
+    double Varinst95;
+    Integer Varinst96;
+    Double Varinst97;
+    Byte Varinst98;
+    Short Varinst99;
+    String Varinst100;
+
+    // class variabel
+    static int Varclass1;
+    static byte Varclass2;
+    static short Varclass3;
+    static double Varclass4;
+    static Integer Varclass5;
+    static Double Varclass6;
+    static Byte Varclass7;
+    static Short Varclass8;
+    static String Varclass9;
+    static int Varclass10;
+    static byte Varclass11;
+    static short Varclass12;
+    static double Varclass13;
+    static Integer Varclass14;
+    static Double Varclass15;
+    static Byte Varclass16;
+    static Short Varclass17;
+    static String Varclass18;
+    static int Varclass19;
+    static byte Varclass20;
+    static short Varclass21;
+    static double Varclass22;
+    static Integer Varclass23;
+    static Double Varclass24;
+    static Byte Varclass25;
+    static Short Varclass26;
+    static String Varclass27;
+    static int Varclass28;
+    static byte Varclass29;
+    static short Varclass30;
+    static double Varclass31;
+    static Integer Varclass32;
+    static Double Varclass33;
+    static Byte Varclass34;
+    static Short Varclass35;
+    static String Varclass36;
+    static int Varclass37;
+    static byte Varclass38;
+    static short Varclass39;
+    static double Varclass40;
+    static Integer Varclass41;
+    static Double Varclass42;
+    static Byte Varclass43;
+    static Short Varclass44;
+    static String Varclass45;
+    static int Varclass46;
+    static byte Varclass47;
+    static short Varclass48;
+    static double Varclass49;
+    static Integer Varclass50;
+    static Double Varclass51;
+    static Byte Varclass52;
+    static Short Varclass53;
+    static String Varclass54;
+    static int Varclass55;
+    static byte Varclass56;
+    static short Varclass57;
+    static double Varclass58;
+    static Integer Varclass59;
+    static Double Varclass60;
+    static Byte Varclass61;
+    static Short Varclass62;
+    static String Varclass63;
+    static int Varclass64;
+    static byte Varclass65;
+    static short Varclass66;
+    static double Varclass67;
+    static Integer Varclass68;
+    static Double Varclass69;
+    static Byte Varclass70;
+    static Short Varclass71;
+    static String Varclass72;
+    static int Varclass73;
+    static byte Varclass74;
+    static short Varclass75;
+    static double Varclass76;
+    static Integer Varclass77;
+    static Double Varclass78;
+    static Byte Varclass79;
+    static Short Varclass80;
+    static String Varclass81;
+    static int Varclass82;
+    static byte Varclass83;
+    static short Varclass84;
+    static double Varclass85;
+    static Integer Varclass86;
+    static Double Varclass87;
+    static Byte Varclass88;
+    static Short Varclass89;
+    static String Varclass90;
+    static int Varclass91;
+    static byte Varclass92;
+    static short Varclass93;
+    static double Varclass94;
+    static Integer Varclass95;
+    static Double Varclass96;
+    static Byte Varclass97;
+    static Short Varclass98;
+    static String Varclass99;
+    static int Varclass100;
+
+    // parameter variabel
+    public void localVar(
+            short VarPara1,
+            double VarPara2,
+            Integer VarPara3,
+            Double VarPara4,
+            Byte VarPara5,
+            Short VarPara6,
+            String VarPara7,
+            int VarPara8,
+            byte VarPara9,
+            short VarPara10,
+            double VarPara11,
+            Integer VarPara12,
+            Double VarPara13,
+            Byte VarPara14,
+            Short VarPara15,
+            String VarPara16,
+            int VarPara17,
+            byte VarPara18,
+            short VarPara19,
+            double VarPara20,
+            Integer VarPara21,
+            Double VarPara22,
+            Byte VarPara23,
+            Short VarPara24,
+            String VarPara25,
+            int VarPara26,
+            byte VarPara27,
+            short VarPara28,
+            double VarPara29,
+            Integer VarPara30,
+            Double VarPara31,
+            Byte VarPara32,
+            Short VarPara33,
+            String VarPara34,
+            int VarPara35,
+            byte VarPara36,
+            short VarPara37,
+            double VarPara38,
+            Integer VarPara39,
+            Double VarPara40,
+            Byte VarPara41,
+            Short VarPara42,
+            String VarPara43,
+            int VarPara44,
+            byte VarPara45,
+            short VarPara46,
+            double VarPara47,
+            Integer VarPara48,
+            Double VarPara49,
+            Byte VarPara50,
+            Short VarPara51,
+            String VarPara52,
+            int VarPara53,
+            byte VarPara54,
+            short VarPara55,
+            double VarPara56,
+            Integer VarPara57,
+            Double VarPara58,
+            Byte VarPara59,
+            Short VarPara60,
+            String VarPara61,
+            int VarPara62,
+            byte VarPara63,
+            short VarPara64,
+            double VarPara65,
+            Integer VarPara66,
+            Double VarPara67,
+            Byte VarPara68,
+            Short VarPara69,
+            String VarPara70,
+            int VarPara71,
+            byte VarPara72,
+            short VarPara73,
+            double VarPara74,
+            Integer VarPara75,
+            Double VarPara76,
+            Byte VarPara77,
+            Short VarPara78,
+            String VarPara79,
+            int VarPara80,
+            byte VarPara81,
+            short VarPara82,
+            double VarPara83,
+            Integer VarPara84,
+            Double VarPara85,
+            Byte VarPara86,
+            Short VarPara87,
+            String VarPara88,
+            int VarPara89,
+            byte VarPara90,
+            short VarPara91,
+            double VarPara92,
+            Integer VarPara93,
+            Double VarPara94,
+            Byte VarPara95,
+            Short VarPara96,
+            String VarPara97,
+            int VarPara98,
+            byte VarPara99,
+            short VarPara100) {
+        
+        public void(){
+            
+        }
+        
+        // class parameter
+        byte VarLocal1;
+        short VarLocal2;
+        double VarLocal3;
+        Integer VarLocal4;
+        Double VarLocal5;
+        Byte VarLocal6;
+        Short VarLocal7;
+        String VarLocal8;
+        int VarLocal9;
+        byte VarLocal10;
+        short VarLocal11;
+        double VarLocal12;
+        Integer VarLocal13;
+        Double VarLocal14;
+        Byte VarLocal15;
+        Short VarLocal16;
+        String VarLocal17;
+        int VarLocal18;
+        byte VarLocal19;
+        short VarLocal20;
+        double VarLocal21;
+        Integer VarLocal22;
+        Double VarLocal23;
+        Byte VarLocal24;
+        Short VarLocal25;
+        String VarLocal26;
+        int VarLocal27;
+        byte VarLocal28;
+        short VarLocal29;
+        double VarLocal30;
+        Integer VarLocal31;
+        Double VarLocal32;
+        Byte VarLocal33;
+        Short VarLocal34;
+        String VarLocal35;
+        int VarLocal36;
+        byte VarLocal37;
+        short VarLocal38;
+        double VarLocal39;
+        Integer VarLocal40;
+        Double VarLocal41;
+        Byte VarLocal42;
+        Short VarLocal43;
+        String VarLocal44;
+        int VarLocal45;
+        byte VarLocal46;
+        short VarLocal47;
+        double VarLocal48;
+        Integer VarLocal49;
+        Double VarLocal50;
+        Byte VarLocal51;
+        Short VarLocal52;
+        String VarLocal53;
+        int VarLocal54;
+        byte VarLocal55;
+        short VarLocal56;
+        double VarLocal57;
+        Integer VarLocal58;
+        Double VarLocal59;
+        Byte VarLocal60;
+        Short VarLocal61;
+        String VarLocal62;
+        int VarLocal63;
+        byte VarLocal64;
+        short VarLocal65;
+        double VarLocal66;
+        Integer VarLocal67;
+        Double VarLocal68;
+        Byte VarLocal69;
+        Short VarLocal70;
+        String VarLocal71;
+        int VarLocal72;
+        byte VarLocal73;
+        short VarLocal74;
+        double VarLocal75;
+        Integer VarLocal76;
+        Double VarLocal77;
+        Byte VarLocal78;
+        Short VarLocal79;
+        String VarLocal80;
+        int VarLocal81;
+        byte VarLocal82;
+        short VarLocal83;
+        double VarLocal84;
+        Integer VarLocal85;
+        Double VarLocal86;
+        Byte VarLocal87;
+        Short VarLocal88;
+        String VarLocal89;
+        int VarLocal90;
+        byte VarLocal91;
+        short VarLocal92;
+        double VarLocal93;
+        Integer VarLocal94;
+        Double VarLocal95;
+        Byte VarLocal96;
+        Short VarLocal97;
+        String VarLocal98;
+        int VarLocal99;
+        byte VarLocal100;
+
+    }
+}
